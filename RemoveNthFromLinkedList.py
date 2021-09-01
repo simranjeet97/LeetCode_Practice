7743a79bba8067961fd24453626ac475768f4538
Simranjeet Singh <38141273+simranjeet97@users.noreply.github.com>
2021-09-02 02:34:00 +0530

    Create RemoveNthFromLinkedList.py

---
+++ RemoveNthFromLinkedList.py
@@ -0,0 +1,16 @@
+class Solution(object):
+    def removeNthFromEnd(self, head, n):
+        dummy = ListNode(0)
+        dummy.next = head
+        first = second = dummy
+        
+        for i in range(n):
+            first = first.next
+        
+        while first.next is not None:
+            first = first.next
+            second = second.next
+        else:
+            second.next = second.next.next
+        
+        return dummy.next
